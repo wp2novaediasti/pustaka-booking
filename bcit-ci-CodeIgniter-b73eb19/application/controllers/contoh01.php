fce3bc1ef48d69c8f3c949ad95d6fb61892a8d0b
wp2novaediasti <nofaediasti@gmail.com>
2021-10-07 21:43:10 +0700

    push pustaka-booking & controller contoh1.php

---
+++ bcit-ci-CodeIgniter-b73eb19/application/controllers/contoh01.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class contoh1 extends CI_Controller 
+{
+	public function index()
+	{
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Nova Ediasti R
+             Saya tinggal di daerah Jogja
+             Olah raga yang saya sukai adalah
+             Basket";
+	}
